137e955faeac62767137ba23a65f57d44222e1b1
LautyDe <lauty.d.p@gmail.com>
2022-06-22 19:22:03 -0300

    guardo archivo donde ira el carrito

---
+++ src/components/ItemListContainer.js
@@ -0,0 +1,9 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+import "./ItemListContainer.css";
+
+function ItemListContainer() {
+  return <h1>Hola</h1>;
+}
+
+export default ItemListContainer;
