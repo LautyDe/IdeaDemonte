ce362b3abc007f162fa6070da37f22d4b37f22c0
LautyDe <lauty.d.p@gmail.com>
2022-06-26 18:01:59 -0300

    creo componente contador

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Navbar from "./components/Navbar";
 import ItemListContainer from "./components/ItemListContainer";
+import Contador from "./components/ItemCount";
+import ItemCount from "./components/ItemCount";
 
 function App() {
   return (
     <div>
       <Navbar />
       <ItemListContainer />
+      <ItemCount />
     </div>
   );
 }
---
+++ src/components/ItemCount.js
@@ -0,0 +1,29 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+import "./ItemCount";
+import { useState } from "react";
+
+function ItemCount() {
+  const [num, setNum] = useState(0);
+  const sumar = () => {
+    setNum(num + 1);
+  };
+  const resta = () => {
+    if (num > 0) {
+      setNum(num - 1);
+    }
+  };
+  const reiniciar = () => {
+    setNum(0);
+  };
+  return (
+    <>
+      <p>{num}</p>
+      <button onClick={resta}>-</button>
+      <button onClick={sumar}>+</button>
+      <button onClick={reiniciar}>Reiniciar</button>
+    </>
+  );
+}
+
+export default ItemCount;
